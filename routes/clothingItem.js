32dea85358ab4fb4dad5ade6f30ba2f477eb637b
Joe Roodvoets <jroodvoets6@gmail.com>
2026-03-09 17:12:05 -0400

    Initial Changes

---
+++ routes/clothingItem.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+const {
+  createItem,
+  getItems,
+  updateItem,
+  deleteItem,
+} = require("../controllers/clothingItem");
+
+//CRUD
+
+//Create
+router.post("/", createItem);
+
+//Read
+
+router.post("/", getItems);
+
+//Update
+
+router.put("/:itemId", updateItem);
+
+//Delete
+
+router.delete("/:itemId", deleteItem);
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const clothingItem = require("./clothingItem");
+
+const userRouter = require("./users");
+router.use("/items", itemRouter);
+router.use("/users", userRouter);
+
+router.use((req, res) => {
+  res.status(500).send({ message: "Router not found" });
+});
+
+module.exports = router;
